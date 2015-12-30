c53c6ca928c2d02d4f5c45aaeff6fc2a0421be20
Dmitry <aksenkin@gmail.com>
2015-12-30 19:46:21 +0300

    attemp to add correction into the launcher file.

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,6 @@
 echo $(pwd)
-ls -o
-echo "raspberry" | sudo -S ./LEDTest -platform xcb
+#ls -o
+echo "rfghbpys" | sudo -S ./LEDTest -platform xcb
+#echo "rfghbpys" |  sudo -S chown root ./LEDTest
+#echo "rfghbpys" |  sudo -S chmod u+s ./LEDTest
+#./LEDTest -platform xcb
